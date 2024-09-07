7da407715f6023edd5f2fea02076a7e0be23f1a4
MyoMyatAung <myatmyo299@gmail.com>
2024-09-07 17:48:35 +0700

    V Card Design

--- src/components/features/qr/VCardPreview.tsx
+++ src/components/features/qr/VCardPreview.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Employee } from '../../../libs/models/qr'
 import { IoPersonCircleOutline } from "react-icons/io5";
+import { MdEmail, MdLocationPin } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoBagSharp } from "react-icons/io5";
+import { FaCircleInfo } from "react-icons/fa6";
 
 type Props = {
   employee: Employee
@@ -9,30 +13,35 @@ type Props = {
 const VCardPreview: React.FC<Props> = ({ employee }) => {
   return (
     <div className='border bg-white'>
-      <div className='w-full h-40 bg-gray-400 flex flex-col items-center justify-center text-white'>
-        <IoPersonCircleOutline className='text-3xl' />
-        <h1>{employee.firstName} {employee.lastName}</h1>
-        <p>{employee.job}</p>
+      <div className='w-full h-40 bg-red-600 flex flex-col items-center justify-center text-white'>
+        <IoPersonCircleOutline className='text-5xl' />
+        <h1 className='text-lg font-semibold my-2'>{employee.firstName} {employee.lastName}</h1>
+        <p className='text-sm text-gray-300'>{employee.job}</p>
       </div>
       <div className="p-2">
-        <div className='my-2'>
-          <p>{employee.email}</p>
+        <div className='my-3 flex items-center gap-3'>
+          <MdEmail />
+          <p className='text-sm'>{employee.email}</p>
         </div>
         <hr />
-        <div className='my-2'>
-          <p>{employee.phone}</p>
+        <div className='my-3 flex items-center gap-3'>
+          <FaPhoneAlt />
+          <p className='text-sm'>{employee.phone}</p>
         </div>
         <hr />
-        <div className='my-2'>
-          <p>{employee.company}</p>
+        <div className='my-3 flex items-center gap-3'>
+          <IoBagSharp />
+          <p className='text-sm'>{employee.company}</p>
         </div>
         <hr />
-        <div className='my-2'>
-          <p>{employee.address}</p>
+        <div className='my-3 flex items-center gap-3'>
+          <MdLocationPin />
+          <p className='text-sm'>{employee.address}</p>
         </div>
         <hr />
-        <div className='my-2'>
-          <p>{employee.summary}</p>
+        <div className='my-3 flex items-center gap-3'>
+          <FaCircleInfo />
+          <p className='text-sm'>{employee.summary}</p>
         </div>
       </div>
     </div>
